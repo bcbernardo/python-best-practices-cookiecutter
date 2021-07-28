6180211e49605a34b20794d248f8fa802d5ea86a
bcbernardo <bc.bernardo@hotmail.com>
2021-07-28 16:52:58 -0300

    Fix license header

--- {{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/tests/conftest.py
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2021 Fiquem Sabendo <contato@fiquemsabendo.com.br>
+# SPDX-FileCopyrightText: {{cookiecutter.year}} {{cookiecutter.author_name}} <{{cookiecutter.author_email}}>
 #
-# SPDX-License-Identifier: NONE
+# SPDX-License-Identifier: {{cookiecutter.license}}
 
 
 import pytest
