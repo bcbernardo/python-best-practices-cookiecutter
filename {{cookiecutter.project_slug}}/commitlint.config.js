120c1bf3803385a3fffc900adbeb5bc33271e6d7
bcbernardo <bc.bernardo@hotmail.com>
2021-07-27 21:13:52 -0300

    Add commitlint pre-commit hook

---
+++ {{cookiecutter.project_slug}}/commitlint.config.js
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: {{cookiecutter.year}} {{cookiecutter.author_name}} <{{cookiecutter.author_email}}>
+//
+// SPDX-License-Identifier: {{cookiecutter.license}}
+
+module.exports = {extends: ['@commitlint/config-conventional']};
